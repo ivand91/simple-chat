795036ee89acef5d8aef30e4d33bd49de629e33b
Ivan <ivan.domazet91@gmail.com>
2018-12-06 20:55:23 +0100

    Config database changes

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'eu-cdbr-west-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'chat'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_8a13b6ef1f908f0'),
+            'username' => env('DB_USERNAME', 'b559f4b5769d13'),
+            'password' => env('DB_PASSWORD', '9af66336'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
